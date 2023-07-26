00acf43c59b35ecd61aa9784ba5376f44f925ed2
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-07-26 21:29:20 +0200

    IdentityDbContext hinzufügen

---
+++ AspNetCoreIdentity.Web/Models/AppDbKontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentity.Web.Models
+{
+    public class AppDbKontext:IdentityDbContext<AppBenutzer,AppRolle,string>
+    {
+        public AppDbKontext(DbContextOptions<AppDbKontext> options) : base(options)
+        {
+
+        }
+    }
+}
--- AspNetCoreIdentity.Web/Program.cs
+++ AspNetCoreIdentity.Web/Program.cs
@@ -1,7 +1,17 @@
+using AspNetCoreIdentity.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbKontext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlVerbindung"));
+});
+
+builder.Services.AddIdentity<AppBenutzer,AppRolle>().AddEntityFrameworkStores<AppDbKontext>();
 
 var app = builder.Build();
 
