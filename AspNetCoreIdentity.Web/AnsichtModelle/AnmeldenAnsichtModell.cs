e9fe57b3be00600b492c80b672cbb260fea8f7d0
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-07-27 21:08:49 +0200

    Entwerfen der Anmeldeseite

---
+++ AspNetCoreIdentity.Web/AnsichtModelle/AnmeldenAnsichtModell.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCoreIdentity.Web.AnsichtModelle
+{
+    public class AnmeldenAnsichtModell
+    {
+        public string? BenutzerName { get; set; }
+        public string? Email { get; set; }
+        public string? Telefonnummer { get; set; }
+        public string? Passwort { get; set; }
+        public string? PasswortBestätigen { get; set; }
+    }
+}
--- AspNetCoreIdentity.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentity.Web/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace AspNetCoreIdentity.Web.Controllers
         {
             return View();
         }
+        public IActionResult Anmelden()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
