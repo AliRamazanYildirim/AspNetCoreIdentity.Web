15baf37977d1cf436a3769a5fe1acc2be9fe5fa3
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-07-30 15:31:37 +0200

    Gestaltung der UserList-Seite-1

---
+++ AspNetCoreIdentity.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCoreIdentity.Web.Areas.Admin.Models;
+using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppBenutzer> _userManager;
+
+        public HomeController(UserManager<AppBenutzer> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> BenutzerListe()
+        {
+            var benutzerListe = await _userManager.Users.ToListAsync();
+            var benutzerAnsichtModellListe = benutzerListe.Select(x => new BenutzerAnsichtModell()
+            {
+                BenutzerID = x.Id,
+                BenutzerName = x.UserName,
+                BenutzerEmail = x.Email
+            }).ToList();
+            return View(benutzerAnsichtModellListe);
+        }
+    }
+}
---
+++ AspNetCoreIdentity.Web/Areas/Admin/Models/BenutzerAnsichtModell.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCoreIdentity.Web.Areas.Admin.Models
+{
+    public class BenutzerAnsichtModell
+    {
+        public string? BenutzerID { get; set; }
+        public string? BenutzerName { get; set; }
+        public string? BenutzerEmail { get; set; }
+
+    }
+}
