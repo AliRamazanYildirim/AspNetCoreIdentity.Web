d8ae3afee013456f81f486d7aa6a63254809f99d
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-08-08 14:35:32 +0200

    Kodierung der Abmeldeaktionsmethode

---
+++ AspNetCoreIdentity.Web/Controllers/MitgliedController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentity.Web.Controllers
+{
+    public class MitgliedController : Controller
+    {
+        private readonly SignInManager<AppBenutzer> _signInManager;
+
+        public MitgliedController(SignInManager<AppBenutzer> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Ausloggen()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
