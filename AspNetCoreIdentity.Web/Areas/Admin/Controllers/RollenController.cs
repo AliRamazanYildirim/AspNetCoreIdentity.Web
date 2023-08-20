4bf593d6c7c245212dd25e2ade7bafcac7491549
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-08-20 10:28:00 +0200

    Autorisierung/Rollenerstellung-1

---
+++ AspNetCoreIdentity.Web/Areas/Admin/Controllers/RollenController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RollenController : Controller
+    {
+        private readonly UserManager<AppBenutzer> _userManager;
+        private readonly RoleManager<AppRolle> _roleManager;
+
+        public RollenController(UserManager<AppBenutzer> userManager, RoleManager<AppRolle> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
