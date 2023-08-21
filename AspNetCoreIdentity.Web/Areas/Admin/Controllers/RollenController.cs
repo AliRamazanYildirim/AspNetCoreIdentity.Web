3bd8f05b4e799d8268f6f986b7321ad6c6d4d1ae
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-08-21 16:00:49 +0200

    Autorisierung/Rollenerstellung-2

--- AspNetCoreIdentity.Web/Areas/Admin/Controllers/RollenController.cs
+++ AspNetCoreIdentity.Web/Areas/Admin/Controllers/RollenController.cs
@@ -1,6 +1,8 @@
-﻿using AspNetCoreIdentity.Web.Models;
+﻿using AspNetCoreIdentity.Web.Areas.Admin.Models;
+using AspNetCoreIdentity.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreIdentity.Web.Erweiterungen;
 
 namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
 {
@@ -19,6 +21,31 @@ namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult RolleErstellen()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> RolleErstellen(RolleErstellenAnsichtModell anfrage)
+        {
+            if (anfrage is null)
+            {
+                throw new ArgumentNullException(nameof(anfrage));
+            }
+
+            var resultat = await _roleManager.CreateAsync(new AppRolle
+            {
+                Name = anfrage.Name
+            });
+
+            if (!resultat.Succeeded) 
+            {
+                ModelState.AddModelStateFehlerListe(resultat.Errors);
+                return View();
+            }
+
+            return RedirectToAction(nameof(RollenController.Index));
         }
     }
 }
---
+++ AspNetCoreIdentity.Web/Areas/Admin/Models/RolleErstellenAnsichtModell.cs
@@ -0,0 +1,7 @@
+﻿namespace AspNetCoreIdentity.Web.Areas.Admin.Models
+{
+    public class RolleErstellenAnsichtModell
+    {
+        public string? Name { get; set; }
+    }
+}
