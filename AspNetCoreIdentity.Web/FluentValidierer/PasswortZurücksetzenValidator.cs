b99f4eff429ff02d524c130cfa191a6d60d60074
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-09-09 10:33:47 +0200

    PasswortZurücksetzenValidator fxieren

--- AspNetCoreIdentity.Web/FluentValidierer/PasswortZurücksetzenValidator.cs
+++ AspNetCoreIdentity.Web/FluentValidierer/PasswortZurücksetzenValidator.cs
@@ -10,11 +10,12 @@ namespace AspNetCoreIdentity.Web.FluentValidierer
             RuleFor(p => p.Passwort).NotEmpty().WithMessage("Ihr neues Passwort darf nicht leer sein.")
                .MinimumLength(8).WithMessage("Ihre Passwortlänge muss mindestens 8 betragen.")
                .MaximumLength(16).WithMessage("Ihre Passwortlänge darf 16 nicht überschreiten.");
-            RuleFor(b => b.Passwort).Matches(@"[A-Z]+").WithMessage("Ihr Passwort muss mindestens einen Großbuchstaben enthalten.");
-            RuleFor(b => b.Passwort).Matches(@"[a-z]+").WithMessage("Ihr Passwort muss mindestens einen Kleinbuchstaben enthalten.");
-            RuleFor(b => b.Passwort).Matches(@"[0-9]+").WithMessage("Ihr Passwort muss mindestens eine Nummer enthalten.");
-            RuleFor(b => b.Passwort).Matches(@"[\!\?\*\.]*$").WithMessage("Ihr Passwort muss mindestens ein (!? *.) enthalten.");
-            RuleFor(x => x.PasswortBestätigen).NotEmpty().WithMessage("Bitte bestätigen Sie Ihr Passwort.");
+            RuleFor(p => p.Passwort).Matches(@"[A-Z]+").WithMessage("Ihr Passwort muss mindestens einen Großbuchstaben enthalten.");
+            RuleFor(p => p.Passwort).Matches(@"[a-z]+").WithMessage("Ihr Passwort muss mindestens einen Kleinbuchstaben enthalten.");
+            RuleFor(p => p.Passwort).Matches(@"[0-9]+").WithMessage("Ihr Passwort muss mindestens eine Nummer enthalten.");
+            RuleFor(p => p.Passwort).Matches(@"[\!\?\*\.]*$").WithMessage("Ihr Passwort muss mindestens ein (!? *.) enthalten.");
+            RuleFor(p => p.Passwort).Equal(pb => pb.PasswortBestätigen).WithMessage("Ihr Passwort stimmen nicht über.");
+
         }
     }
 }
