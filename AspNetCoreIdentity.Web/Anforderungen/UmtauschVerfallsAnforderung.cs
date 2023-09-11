c99578db2e5ce41051fc66b1b8883b353bf3d90e
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-09-11 20:36:07 +0200

    Szenario 2 (Zugriff auf eine Seite für eine bestimmte Zeit) – AuthorizationHandler

---
+++ AspNetCoreIdentity.Web/Anforderungen/UmtauschVerfallsAnforderung.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace AspNetCoreIdentity.Web.Anforderungen
+{
+    public class UmtauschVerfallsAnforderung:IAuthorizationRequirement
+    {
+    }
+
+    public class UmtauschVerfallsAnforderungHandler : AuthorizationHandler<UmtauschVerfallsAnforderung>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, UmtauschVerfallsAnforderung requirement)
+        {
+            if(context.User.HasClaim(x => x.Type == "AblaufDatumDesUmtauschs"))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+            Claim umtauschClaimAblaufdatum = context.User.FindFirst( "AblaufDatumDesUmtauschs")!;
+            if (DateTime.Now > Convert.ToDateTime(umtauschClaimAblaufdatum.Value))
+            {
+                context.Fail(); 
+                return Task.CompletedTask;
+            }
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+
+        }
+    }
+}
